1d81d35a5501881f35b0980bceda646666b18b30
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-01-17 06:37:18 -0500

    updates

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/04_Operators, Assignment, and Arithmetic/script.js
@@ -0,0 +1,15 @@
+let number1 = prompt("Enter a number: ");
+let number2 = prompt("Enter another number: ");
+
+number1 = +number1;
+number2 = +number2;
+
+// console.log(typeof(number1));
+
+sum = number1 + number2;
+//dif
+//prod
+//quo
+//mod
+
+document.getElementById("sum").innerHTML = sum;
