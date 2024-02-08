81f72c731c655c04393498e20fb8f9a3a6eef32a
Xavier Harding <hardixav000@warren.k12.in.us>
2024-02-08 11:51:38 -0500

    beats

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -15,3 +15,22 @@ function takeAbsoluteValue() {
     let absoluteValue = Math.abs(number);
     document.getElementById("result2").innerHTML = absoluteValue;
 }
+
+function takeSine() {
+    let number = +document.getElementById("input3").value;
+
+    let sine = Math.sin(number * Math.PI / 180);
+    document.getElementById("result3").innerHTML = sine;
+}
+
+function takeCosine() {
+    let number = +document.getElementById("input4").value;
+
+    let cosine = Math.cos(number * Math.PI / 180);
+    document.getElementById("result4").innerHTML = cosine;
+}
+
+function takeRandom(){
+    document.getElementById("result5").innerHTML = Math.random();
+
+}
