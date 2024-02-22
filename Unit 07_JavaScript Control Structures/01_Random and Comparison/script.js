f88901945a3a5f92a8f346d2490f827359ffd0c1
Xavier Harding <hardixav000@warren.k12.in.us>
2024-02-22 11:46:33 -0500

    music :)

--- Unit 07_JavaScript Control Structures/01_Random and Comparison/script.js
+++ Unit 07_JavaScript Control Structures/01_Random and Comparison/script.js
@@ -9,8 +9,15 @@ function Roll() {
         diceElement.src = "images/dice1.jpg";
     } else if (random === 2) {
         diceElement.src = "images/dice2.jpg";
+    }  else if (random === 3) {
+        diceElement.src = "images/dice3.jpg";
+    }  else if (random === 4) {
+        diceElement.src = "images/dice4.jpg";
+    }  else if (random === 5) {
+        diceElement.src = "images/dice5.jpg";
+    }  else if (random === 6) {
+        diceElement.src = "images/dice6.jpg";
     }
-    //TODO:  YOU do 3 to 6
 }
 
 function RollSwitch() {
