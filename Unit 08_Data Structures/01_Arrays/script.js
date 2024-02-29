f5cc753da65609cd5950967ed245118f925d317b
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-29 08:39:17 -0500

    updates

---
+++ Unit 08_Data Structures/01_Arrays/script.js
@@ -0,0 +1,9 @@
+const numbers = []
+
+for(let i = 0; i < 10; i++){
+    let num = Math.floor(Math.random() * 10) + 1;
+    numbers.push(num);
+}
+
+document.getElementById("unsorted").innerHTML = numbers;
+
